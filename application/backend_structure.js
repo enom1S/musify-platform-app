913ae6433bd60da2be7e874b12aafb09bfd96e6a
GRRSMN01E01I754J <GRRSMN01E01I754J@studium.unict.it>
2025-08-17 12:24:07 +0200

    feat: first ci-cd release

--- application/backend_structure.js
+++ application/backend_structure.js
@@ -1574,7 +1574,7 @@ app.get('/api/users/:userId/history/stats', authenticateToken, async (req, res) 
 });
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:8080', 'http://127.0.0.1:5500'],
+  origin: ['http://0.0.0.0:3000', 'http://0.0.0.0:8080', 'http://0.0.0.0:5500'],
   credentials: true
 }));
 
